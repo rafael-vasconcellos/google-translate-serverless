5fe8adffb31a95792377a149ea21b735cae567db
Rafael Vasconcelos <rafael.vasconcelos7497@gmail.com>
2025-06-03 23:35:30 +0000

    enhance ip address checker test

--- api/ip/index.ts
+++ api/ip/index.ts
@@ -1,11 +1,31 @@
 import { VercelRequest, VercelResponse } from "@vercel/node";
-import axios, { AxiosResponse } from "axios";
+//import axios, { AxiosResponse } from "axios";
+import { IncomingHttpHeaders } from 'http'
 
+
+function incomingHttpHeadersToHeadersInit(incomingHttpHeaders: IncomingHttpHeaders) { 
+    const headers = new Headers()
+    for (const [key, value] of Object.entries(incomingHttpHeaders)) {
+        if (Array.isArray(value)) {
+            for (const v of value) {
+            headers.append(key, v);
+            }
+        } else if (value !== undefined) {
+            headers.set(key, value);
+        }
+    }
+    return headers
+}
 
 export default async function handler(req: VercelRequest, res: VercelResponse) { 
-    const response = await axios.get('https://api.ipify.org?format=json')
+    const headers = incomingHttpHeadersToHeadersInit(req.headers)
+    const response = await fetch('https://api.ipify.org?format=json', { 
+        //headers,
+    })
+    //.then(response => response.data)
+    .then(response => response.json())
     .catch(err => err);
 
     if (response instanceof Error) { return res.status(500).send('Error: ' + response.message) }
-    return new Response('IP address: ' + response.data.ip)
+    res.status(200).send(response.ip)
 }
